140d155b43ed2caf5478df4dcad5d3826fa3e9b9
Lê Việt Thanh - edu <thanhlv@1school.edu.vn>
2025-11-13 01:22:17 +0700

    Update CourseCard components on HomePage
    Removed onClick handlers from CourseCard components for 'An toàn Bức xạ' and 'Quan trắc Môi trường'.

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -216,8 +216,8 @@ const HomePage: React.FC<HomePageProps> = ({ onNavigate }) => {
                   <CourseCard icon="fa-hard-hat" title="An toàn Xây dựng" onClick={() => onNavigate('training-an-toan-xay-dung')} gradient="from-orange-500 to-orange-700" />
                   <CourseCard icon="fa-flask" title="An toàn Hóa chất" onClick={() => onNavigate('training-an-toan-hoa-chat')} gradient="from-purple-500 to-purple-700" />
                   <CourseCard icon="fa-fire-extinguisher" title="PCCC" onClick={() => onNavigate('training-pccc')} gradient="from-red-500 to-red-700" />
-                  <CourseCard icon="fa-radiation-alt" title="An toàn Bức xạ" onClick={() => onNavigate('training-an-toan-buc-xa')} gradient="from-cyan-500 to-cyan-700" />
-                  <CourseCard icon="fa-leaf" title="Quan trắc Môi trường" onClick={() => onNavigate('training-quan-trac-moi-truong')} gradient="from-green-500 to-green-700" />
+                  <CourseCard icon="fa-radiation-alt" title="An toàn Bức xạ" />
+                  <CourseCard icon="fa-leaf" title="Quan trắc Môi trường" />
                   <CourseCard icon="fa-clipboard-check" title="Đánh giá Phân loại Lao động" onClick={() => onNavigate('training-danh-gia-phan-loai-lao-dong')} gradient="from-indigo-500 to-indigo-700" />
                   <CourseCard icon="fa-medkit" title="Sơ Cấp Cứu" onClick={() => onNavigate('training-so-cap-cuu')} gradient="from-pink-500 to-pink-700" />
               </div>
